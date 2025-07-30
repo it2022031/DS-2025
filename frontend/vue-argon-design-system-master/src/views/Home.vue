1139efcd62e1833d99ad4e026712a90dcce96996
it2022129 <it2022129@hua.gr>
2025-07-30 15:52:28 +0300

    αλλαγές στο vue του Home

--- frontend/vue-argon-design-system-master/src/views/Home.vue
+++ frontend/vue-argon-design-system-master/src/views/Home.vue
@@ -3,10 +3,12 @@
     <h1>🏠 Καλώς ήρθες στην εφαρμογή!</h1>
     <p>Επέλεξε τι θέλεις να κάνεις:</p>
 
+    <router-link to="/properties/add" class="btn btn-success m-2">➕ Add a Property</router-link>
+    <router-link to="/rentals" class="btn btn-info m-2">📃 Rentals List</router-link>
+    <router-link to="/rent" class="btn btn-warning m-2">📅 Rent a Property</router-link>
     <router-link to="/profile" class="btn btn-primary m-2">👤 Profile</router-link>
-    <router-link to="/properties/add" class="btn btn-success m-2">➕ New Ad</router-link>
-    <router-link to="/list-properties" class="btn btn-info m-2">📃 Rentals List</router-link>
-    <router-link to="/rentals" class="btn btn-warning m-2">📅 Rent a Property</router-link>
+    <router-link to="/about" class="btn btn-warning m-2">About us</router-link>
+
   </div>
 </template>
 
