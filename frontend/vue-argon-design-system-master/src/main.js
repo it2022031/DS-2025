a8a3899a12db7bc58d73ad32d39a0d8c748a5feb
IOANNIS KACHRAMANOS <it2022034@hua.gr>
2025-08-28 14:52:22 +0300

    Frontend - Auto Logout on Start Added (main.js)

--- frontend/vue-argon-design-system-master/src/main.js
+++ frontend/vue-argon-design-system-master/src/main.js
@@ -11,6 +11,18 @@ Vue.use(Argon);
 // διαθέσιμο σε όλα τα components ως this.$api
 Vue.prototype.$api = api;
 
+// 🔹 Logout only on first app open (per browser session)
+if (!sessionStorage.getItem("appStarted")) {
+    localStorage.removeItem("token");
+    localStorage.removeItem("token_expiry");
+    localStorage.removeItem("userRoles");
+    localStorage.removeItem("userRole");
+    localStorage.removeItem("username");
+    localStorage.removeItem("userId");
+
+    sessionStorage.setItem("appStarted", "true");
+}
+
 new Vue({
     router,
     render: h => h(App)
