c20d98636270784e9bde357c10a9e8de09eb9f45
IOANNIS KACHRAMANOS <it2022034@hua.gr>
2025-08-29 15:23:29 +0300

    Frontend - New Logo Added, Handled

--- frontend/vue-argon-design-system-master/src/layout/AppHeader.vue
+++ frontend/vue-argon-design-system-master/src/layout/AppHeader.vue
@@ -2,8 +2,8 @@
   <header class="header-global">
     <base-nav ref="nav" class="navbar-main" transparent effect="light" expand>
       <!-- Λογότυπο -->
-      <router-link slot="brand" class="navbar-brand mr-lg-5" to="/">
-        <img src="img/brand/white.png" alt="logo" />
+      <router-link slot="brand" class="navbar-brand " to="/">
+        <img src="img/brand/logo_font.png" alt="logo" />
       </router-link>
 
       <!-- Mobile menu header -->
